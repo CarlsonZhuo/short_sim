7e641a973a47b19d6414efac83f6541abef899c7
Jiacheng Zhuo <5838502+CarlsonZhuo@users.noreply.github.com>
2019-04-23 11:08:52 -0500

    Update Huge_Gausian_Clustering.py

--- Huge_Gausian_Clustering.py
+++ Huge_Gausian_Clustering.py
@@ -2,19 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pdb
 
-p = 200
-mean = [0] * p
-cov = np.eye(p)*1
+p = 100
+mean1 = [-p] * p
+cov1 = np.eye(p)*1
 
-X = np.random.multivariate_normal(mean, cov, 500).T
-XTX = X.T.dot(X)
-diag_XTX = np.diagonal(XTX)
-plt.plot(np.sort(diag_XTX))
-plt.show()
+mean2 = [p] * p
+cov2 = np.eye(p)*1
 
-off_diag_XTX = XTX - np.diag(diag_XTX)
-vecXTX = off_diag_XTX.reshape((1,-1))[0]
-vecXTX = np.sort(vecXTX)
-plt.plot(vecXTX)
-plt.show()
-pdb.set_trace()
+Y = np.random.multivariate_normal(mean2, cov2, 2000).T
+X = np.random.multivariate_normal(mean1, cov1, 2000).T
+
+def XTX(X):
+    XTX = X.T.dot(X)
+    diag_XTX = np.diagonal(XTX)
+    # plt.hist(diag_XTX, bins=30)
+    # plt.show()
+
+    off_diag_XTX = XTX - np.diag(diag_XTX)
+    vecXTX = off_diag_XTX.reshape((1,-1))[0]
+    plt.hist(vecXTX, bins=100)
+    plt.show()
+
+def XTY(X, Y):
+    XTY = X.T.dot(Y)
+    vecXTY = XTY.reshape((1,-1))[0]
+    plt.hist(vecXTY, bins=100)
+    plt.show()
+
+XTX(X)
+XTY(X, Y)
